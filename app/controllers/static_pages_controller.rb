3f42eac8352e15062a08b464d610b87da1c67400
Luke Wright <luke@simplyluke.com>
2016-04-16 18:39:11 -0500

    update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,14 @@
-
-
 class StaticPagesController < ApplicationController
   def home
 
     @stock = Stock.last
+  end
+
+  def chart
+    @stocks = Stock.where("created_at >= ?", Time.zone.now.beginning_of_day)
+    respond_to do |format|
+      format.html
+      format.json {render json:@stocks}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
+  get 'chart' => 'static_pages#chart'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
