7563defdbdafe52db36c4109c671686aa96fdb74
Luke Wright <luke@simplyluke.com>
2016-04-16 23:56:05 -0500

    headlines

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-
+    @weather = JSON.load(open("https://api.forecast.io/forecast/c227273194d05f8e76185ef0240d8b42/32.7828508,-96.776557"))
+    @headlines = JSON.load(open('http://api.nytimes.com/svc/news/v3/content/all/national,world/.json?limit=6&api-key=3e1ff7ce6406b793cb9adc2f4bb54f03%3A8%3A75020917'))
     @stock = Stock.last
   end
 
